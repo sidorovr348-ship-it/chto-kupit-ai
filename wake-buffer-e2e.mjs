b5bc8651b4182a444534f939fe9a15b7031f3a54
sidorovr348-ship-it <sidorovr348@gmail.com>
2026-09-26 19:44:51 +0300

    test: simulate iOS wake session ending after each command

--- wake-buffer-e2e.mjs
+++ wake-buffer-e2e.mjs
@@ -35,6 +35,7 @@ const API='https://ai.aliceq.ru';
         setTimeout(()=>emit(command[0]),150);
         setTimeout(()=>emit(command[1]),300);
         setTimeout(()=>emit(command[2]),450);
+        setTimeout(()=>this.onend?.(),1800);
       }
     }
     window.SpeechRecognition=FakeSpeechRecognition;
